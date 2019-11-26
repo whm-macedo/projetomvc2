cde758d01037dd5e6146ab598be34a849dd8f1f1
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-11-26 13:52:50 -0300

    Model Carrinho

--- Model/Carrinho.php
+++ Model/Carrinho.php
@@ -1,6 +1,8 @@
 <?php
 namespace LOJA\Model;
 use LOJA\Model\Item;
+use LOJA\Model\Produto;
+use LOJA\DAO\DAOProduto;
 
 class Carrinho{
     private $id;
@@ -17,5 +19,28 @@ class Carrinho{
     public function setId()
     {
         $this->id = $id;
+    }
+
+    public function addItem($id)
+    {
+        $dao = new DAOProduto();
+        $obj = new Produto();
+        $obj = $dao->buscaPorId($id);
+
+        if($obj->getId()){
+            $item = new Item();
+            $item->setProduto($obj);
+            $item->setQuantidade(1);
+        };
+        array_push($this->lista, $item);
+    }
+    public function removeItem($id)
+    {
+        foreach ($this->lista as $item) {
+            if ($item->getProduto()->gitId()===$id) {
+                $index = array_search($item, $this->lista, true);
+                unset($this->lista[$index]);
+            };
+        }
     }
 }
